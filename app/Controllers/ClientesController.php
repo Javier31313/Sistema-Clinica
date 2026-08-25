d8b4eb33d3f2b0ae8291acb13e4a3d333b984f81
Javier31313 <josh95x2@gmail.com>
2026-08-24 21:24:53 -0600

    Refactor ClientesController to improve obtener_clientes

--- app/Controllers/ClientesController.php
+++ app/Controllers/ClientesController.php
@@ -32,7 +32,7 @@
             $count = count($registros);
 
             // Formula para determinar la cantidad de páginas
-            $total_pages = $count > 0 ? ceil($count/$limi) : 0;
+            $total_pages = $count > 0 ? ceil($count/$limit) : 0;
 
             // Condición para verificar si no hay mas de 1 página
             if ($page > $total_pages) $page = $total_pages;
@@ -51,8 +51,8 @@
             // Preparamos los datos que se mostraran en la tabla dinamica
             $i = 0;
             foreach($registros as $cliente) {
-                $responce->rows[$i]['id'] = $cliente->id_cliente;
-                $responce->rows[$i]['cell'] = [$cliente->nombre_empresa, $cliente->correo_empresarial, $cliente->telefono_empresa, $cliente->persona_contacto, $cliente->telefono_contacto];
+                $responce->rows[$i]['id'] = $cliente['id'];
+                $responce->rows[$i]['cell'] = [$cliente['nombre_empresa'], $cliente['correo_empresarial'], $cliente['telefono_empresa'], $cliente['persona_contacto'], $cliente['correo_contacto'], $cliente['telefono_contacto'], $cliente['celular_contacto'], $cliente['fecha_creacion'], $cliente['fecha_modificacion']];
                 $i++;
             }
             
@@ -62,40 +62,4 @@
 
     }
 
-/*
-
-namespace App\Controllers;
-
-use App\Models\ClienteModel;
-
-class ClientesController extends Controller { // extendemos a Controller para usar el método view()
     
-    protected $model;
-
-    public function __construct() {
-        $this->model = new ClienteModel();
-    }
-
-    public function index() {
-        return $this->view('clientes'); // el parametro que recibe view es la ruta o el nombre de el archivo clientes.php
-    }
-
-    public function index2() {
-        return $this->view('clientesDashboard');
-    }
-
-    public function obtener_registros() {
-        // $model2 = new ClienteModel();
-
-        // return $model2->where('id',5)->first();
-            
-
-        //Este que hizo el profe funciona sin tener que YO desde AQUI escribir una consulta sql porque el metodo all ya la trae ya lo tiene definido, por eso cuando usas el metodo first no funciona porque no existe ninguna consulta sql A MENOS que la hagas desde aqui, como lo haces con tu prueba de arriba
-        $registros = $this->model->create([
-            'nombre_empresa' => 'GUCCI',
-            'correo_empresarial' => 'GUCCI',
-            'telefono_empresa' => '173498314789'
-        ]);
-        echo json_encode($registros);
-    }
-}  */
