845c498744b1e2ba8a34d14c01eb8dce1bac543d
Javier31313 <josh95x2@gmail.com>
2026-09-01 10:41:31 -0600

    Delete app directory

--- app/Models/Contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-
-class Contact extends Model{
-    
-    protected $table = 'clientes';
-}
--- app/Models/UsuarioModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Models;
-
-    class UsuarioModel extends Model {
-        protected $table = ('usuarios');
-    }
